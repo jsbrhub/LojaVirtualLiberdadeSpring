8733a9fed6c5195d60a6c9fe10fddfcad865b88c
Jessyca de Oliveira Borges <jessyca.borges@tecnogroup.com.br>
2022-08-11 22:20:59 -0300

    first commit

---
+++ src/main/java/com/lojaliberdade/lojaliberdade/LojaLiberdadeApplication.java
@@ -0,0 +1,13 @@
+package com.lojaliberdade.lojaliberdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaLiberdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaLiberdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojaliberdade/lojaliberdade/LojaLiberdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaliberdade.lojaliberdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaLiberdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
